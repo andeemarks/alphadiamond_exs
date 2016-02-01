4fd40ef370106ad997486c7e81d79dc11edebfd7
Andy Marks <amarks@thoughtworks.com>
2016-02-01 21:14:30 +1100

    First working tests

--- lib/alphadiamond_exs.ex
+++ lib/alphadiamond_exs.ex
@@ -1,2 +1,8 @@
 defmodule ALPHADIAMOND do
+	def is_valid_spec?(spec) when is_binary(spec) do
+		Regex.match?(~r/^[A-Za-z]$/, String.strip(spec))
+	end
+	def is_valid_spec?(_) do
+		false
+	end
 end
--- test/alphadiamond_exs_test.exs
+++ test/alphadiamond_exs_test.exs
@@ -2,7 +2,66 @@ defmodule ALPHADIAMONDTest do
   use ExUnit.Case
   doctest ALPHADIAMOND
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+# 	?_assert(status_after_spec_validation("A")),
+# 	?_assert(status_after_spec_validation('A')),
+# 	?_assert(status_after_spec_validation("c")),
+# 	?_assert(status_after_spec_validation("  E   ")),
+  test "spec validation with valid specs" do
+  	assert ALPHADIAMOND.is_valid_spec?("A")
+  	assert ALPHADIAMOND.is_valid_spec?("c")
+  	assert ALPHADIAMOND.is_valid_spec?("    E       ")
+  end
+
+# 	?_assertNot(status_after_spec_validation("AA")),
+# 	?_assertNot(status_after_spec_validation(" ")),
+# 	?_assertNot(status_after_spec_validation("4")),
+# 	?_assertNot(status_after_spec_validation("{")),
+# 	?_assertNot(status_after_spec_validation(""))
+  test "spec validation with invalid specs" do
+  	refute ALPHADIAMOND.is_valid_spec?("AA")
+  	refute ALPHADIAMOND.is_valid_spec?('A')
+  	
   end
 end
+
+# positive_smoke_test_() -> [
+#     ?_assertEqual(ok, alphadiamond_app:diamond(['A'])),
+#     ?_assertEqual(ok, alphadiamond_app:diamond(["A"]))
+# ].	
+
+# negative_smoke_test_() -> [
+# 	?_assertEqual(error, alphadiamond_app:diamond([";"])),
+# 	?_assertEqual(error, alphadiamond_app:diamond()),
+# 	?_assertEqual(error, alphadiamond_app:diamond([]))
+# ].
+
+# row_after_strip(Letter) -> string:strip(alphadiamond_app:row_for(Letter)).
+
+# row_test_() -> [
+# 	?_assertEqual("A", row_after_strip("A")),
+# 	?_assertEqual("B B", row_after_strip("B")),
+# 	?_assertEqual("J                 J", row_after_strip("J"))
+# ].
+
+# row_instructions_test_() -> [
+# 	?_assertEqual("A", alphadiamond_app:row_instructions_for('A')),
+# 	?_assertEqual("ABA", alphadiamond_app:row_instructions_for('B')),
+# 	?_assertEqual("ABCBA", alphadiamond_app:row_instructions_for('C'))
+# ].
+
+# status_after_spec_validation(Spec) ->
+# 	{SpecStatus, _} = alphadiamond_app:is_valid_spec(Spec),
+# 	SpecStatus.
+
+# valid_spec_test_() -> [
+# 	?_assert(status_after_spec_validation("A")),
+# 	?_assert(status_after_spec_validation('A')),
+# 	?_assert(status_after_spec_validation("c")),
+# 	?_assert(status_after_spec_validation("  E   ")),
+# 	?_assertNot(status_after_spec_validation("AA")),
+# 	?_assertNot(status_after_spec_validation(" ")),
+# 	?_assertNot(status_after_spec_validation("4")),
+# 	?_assertNot(status_after_spec_validation("{")),
+# 	?_assertNot(status_after_spec_validation(""))
+# ].
