027150d8acd32c03abf55dc36ea8caa6f0345c46
Andy Marks <amarks@thoughtworks.com>
2016-02-07 19:55:38 +1100

    row_for now working

--- lib/alphadiamond_exs.ex
+++ lib/alphadiamond_exs.ex
@@ -1,6 +1,7 @@
 defmodule ALPHADIAMOND do
 
 	@alphabet Enum.to_list(?A .. ?Z)
+	@row_template List.to_string(Enum.to_list(?Z .. ?B) ++ Enum.to_list(?A .. ?Z))
 
 	def is_valid_spec?(spec) when is_binary(spec) do
 		Regex.match?(~r/^[A-Za-z]$/, String.strip(spec))
@@ -12,5 +13,10 @@ defmodule ALPHADIAMOND do
 	def row_instructions_for(letter) do
 		instruction_half = List.first(String.split(List.to_string(@alphabet), letter))
 		instruction_half <> letter <> String.reverse(instruction_half)
+	end
+
+	def row_for(letter) do
+		everything_but_letter = elem(Regex.compile("[^" <> letter <> "]"), 1)
+		Regex.replace(everything_but_letter, @row_template, " ")
 	end
 end
--- test/alphadiamond_exs_test.exs
+++ test/alphadiamond_exs_test.exs
@@ -2,11 +2,6 @@ defmodule ALPHADIAMONDTest do
   use ExUnit.Case
   doctest ALPHADIAMOND
 
-# row_instructions_test_() -> [
-# 	?_assertEqual("A", alphadiamond_app:row_instructions_for('A')),
-# 	?_assertEqual("ABA", alphadiamond_app:row_instructions_for('B')),
-# 	?_assertEqual("ABCBA", alphadiamond_app:row_instructions_for('C'))
-# ].
   test "row instructions are valid for letter" do
 		assert ALPHADIAMOND.row_instructions_for("A") == "A"  	
 		assert ALPHADIAMOND.row_instructions_for("B") == "ABA"  	
@@ -27,6 +22,12 @@ defmodule ALPHADIAMONDTest do
   	refute ALPHADIAMOND.is_valid_spec?("")
   	
   end
+
+  test "row construction" do
+		assert String.strip(ALPHADIAMOND.row_for("A")) == "A"
+		assert String.strip(ALPHADIAMOND.row_for("B")) == "B B"
+		assert String.strip(ALPHADIAMOND.row_for("J")) == "J                 J"
+  end
 end
 
 # positive_smoke_test_() -> [
@@ -42,10 +43,5 @@ end
 
 # row_after_strip(Letter) -> string:strip(alphadiamond_app:row_for(Letter)).
 
-# row_test_() -> [
-# 	?_assertEqual("A", row_after_strip("A")),
-# 	?_assertEqual("B B", row_after_strip("B")),
-# 	?_assertEqual("J                 J", row_after_strip("J"))
-# ].
 
 
